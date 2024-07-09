f593a3e2828a995573b4ad146aedc2a0615e7800
SawantAchal <achals1718@gmail.com>
2024-07-09 14:24:59 +0530

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
-
+import CartPage from './pages/CartPage'
+import HomePage from './pages/HomePage'
+import PlaceOrder from './pages/PlaceOrderPage'
 function App() {
   return (
-    <>
+    <div className='App'>
       <Navbar/>
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/cart'  element={<CartPage/>} />
+        <Route path='/order'  element={<PlaceOrder/>} />
+      </Routes>
+    </div>
   )
 }
 
