ae8cf7ba3bfb912d56a047eac25ecd0b21aa4de1
SawantAchal <achals1718@gmail.com>
2024-07-13 01:31:21 +0530

    add storeContext and browser rounter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   // enclose complete app to ther browser router
   <BrowserRouter>
-    <App />
+  {/* App component rap to the storeContect provider */}
+    <StoreContextProvider>
+      <App/>
+    </StoreContextProvider>
   </BrowserRouter>
   // </React.StrictMode>,
 )
