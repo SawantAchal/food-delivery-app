e62c57ce85e376fcce696141875e95534c90ae6e
SawantAchal <achals1718@gmail.com>
2024-07-13 04:18:35 +0530

    add condition to display dot

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import {assets} from '../assets/assets'
 import {Link} from 'react-router-dom'
+import { StoreContext } from '../context/StoreContext';
 
-function Navbar() {
+function Navbar({setShowLogin}) {
   const [menu, setMenu] = useState("home");
+  // to get total cart item
+  const {getTotalCartAmount} = useContext(StoreContext)
 
   return (
     <nav className='p-5 flex justify-between items-center'>
-      <img src={assets.logo} alt='logo' className='md:w-36 w-24 ' />
+      <Link to={'/'}><img src={assets.logo} alt='logo' className='md:w-36 w-24 ' /></Link>
       <ul className='lg:flex list-none gap-5 text-2xl hidden'>
         <Link to= '/' onClick={() => setMenu("home")} className={`cursor-pointer ${menu === "home" ? "border-b-2 border-blue-500" : ""}`}>home</Link>
         <Link to='/menu' onClick={() => setMenu("menu")} className={`cursor-pointer ${menu === "menu" ? "border-b-2 border-blue-500" : ""}`}>menu</Link>
@@ -17,10 +20,12 @@ function Navbar() {
       <section className='flex items-center md:gap-10 gap-5'>
         <img src={assets.search_icon} alt='' />
         <section className='relative'>
-          <img src={assets.basket_icon} alt='' />
-          <section className='absolute min-w-2 min-h-2 bg-red-500 -top-2 -right-2 rounded-lg'></section>
+          <Link to={'/cart'}><img src={assets.basket_icon} alt='' /></Link>
+          {/* if item is in the cart then dot is visible if not then dot is not visible  */}
+          <section className={getTotalCartAmount()===0? '' :'absolute min-w-2 min-h-2 bg-red-500 -top-2 -right-2 rounded-lg' }></section>
         </section>
-        <button className='bg-transparent md:text-lg text-sm border-red-500 p-1 rounded-lg border-2 cursor-pointer'>sign in</button>
+        {/* use Props here  when onclick sign in button*/}
+        <button className='bg-transparent md:text-lg text-sm border-red-500 p-1 rounded-lg border-2 cursor-pointer' onClick={() => setShowLogin(true)}>sign in</button>
       </section>
     </nav>
   );
