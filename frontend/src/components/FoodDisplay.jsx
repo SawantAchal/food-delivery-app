edc32cfffc723f9f27df4f2e314cb6993ea66ab7
SawantAchal <achals1718@gmail.com>
2024-07-12 19:24:05 +0530

    created component to display food item . mount FoodItem Component to display the food item

---
+++ frontend/src/components/FoodDisplay.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import {StoreContext} from '../context/StoreContext'
+import FoodItem from './FoodItem'
+
+const FoodDisplay = ({category}) => {
+    // diplay food list using context api
+    const {food_list} = useContext(StoreContext)
+  return (
+    <div className='mt-8'>
+        <h1 className='text-[max(2vw,24px)] font-bold'>Top dished near you</h1>
+        <div className='grid grid-cols-auto-fill-240 gap-8 mt-8 gap-y-12'>
+            {
+                food_list.map((item, index) => {
+                    return (
+                        <FoodItem key={index} id={item._id} image={item.image} name={item.name} description={item.description} price={item.price} category={item.category}/>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
