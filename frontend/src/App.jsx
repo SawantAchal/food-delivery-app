7f51c52758269d0ee7387101c24b2de034c2a42e
SawantAchal <achals1718@gmail.com>
2024-07-13 01:29:00 +0530

    mounted a component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import Navbar from './components/Navbar'
 import CartPage from './pages/CartPage'
 import HomePage from './pages/HomePage'
 import PlaceOrder from './pages/PlaceOrderPage'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/cart'  element={<CartPage/>} />
-        <Route path='/order'  element={<PlaceOrder/>} />
-      </Routes>
-    </div>
+    <>
+      <div className='App'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/cart'  element={<CartPage/>} />
+          <Route path='/order'  element={<PlaceOrder/>} />
+        </Routes>
+      </div>
+      <Footer/>
+    </>
   )
 }
 
 export default App
+3
