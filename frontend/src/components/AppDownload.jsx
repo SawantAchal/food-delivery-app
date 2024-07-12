e55d649f0871acee1ae44b0a5dcf963c4338a06e
SawantAchal <achals1718@gmail.com>
2024-07-13 00:49:03 +0530

    App download layout

---
+++ frontend/src/components/AppDownload.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const AppDownload = () => {
+  return (
+    <div className='m-auto mt-20 text-[max(3vw, 20px)] text-center font-medium'>
+        <p>For Better Experience Download <br/> Tomato App</p>
+        <div className='flex justify-center gap-[max(2vw ,10px)] mt-10'>
+            <img src={assets.play_store} alt='Play Store' className='w-[max(30vw , 120px)] max-w-44 transition duration-500 cursor-pointer hover:scale-110'/>
+            <img src={assets.app_store} alt='App Store' className='w-[max(30vw , 120px)] max-w-44 transition duration-500 cursor-pointer hover:scale-110'/>
+        </div>
+    </div>
+  )
+}
+
+export default AppDownload
