4ac381433b03f38554ec6fce0339b4da3b75f75c
SawantAchal <achals1718@gmail.com>
2024-07-21 01:56:04 +0530

    created a logic and api call for login and register and save the genrated token in local sorage

--- frontend/src/components/LoginPopup.jsx
+++ frontend/src/components/LoginPopup.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import { StoreContext } from '../context/StoreContext'
+import axios from 'axios';
 
 const LoginPopup = ({setShowLogin}) => {
+    const {url,setToken, token} = useContext(StoreContext)
+
     // to check it is signup form or login form
     const [currentState , setCurrentState] = useState("Sign Up")
     //to store user name and password
@@ -18,9 +22,38 @@ const LoginPopup = ({setShowLogin}) => {
         setData(data => ({...data,[name]:value}))
     }
 
+    //function for login 
+    const onLogin = async (event) =>{
+        event.preventDefault();
+        //logic to call the api 
+        let newUrl = url;
+        //check current state is login 
+        if (currentState === 'Login') {
+            newUrl += '/api/user/login'
+        }
+        else {
+            newUrl += '/api/user/register'
+        }
+        // console.log("Final URL: ", newUrl);
+        // /call api
+        const response = await axios.post(newUrl , data);
+        if (response.data.success) {
+            setToken(response.data.token);
+            // store the token in localstorage
+            localStorage.setItem("token" , response.data.token)
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message) 
+        }
+
+    }
+
+
+
   return (
     <div className='absolute z-10 w-full h-full bg-slate-50 grid'>
-        <form className='place-self-center w-[max(23vw,330px)] text-gray-400 bg-white flex flex-col gap-6 rounded-md text-lg animate-[fadeIn_0.5s] py-6 px-8'>
+        <form onSubmit={onLogin} className='place-self-center w-[max(23vw,330px)] text-gray-400 bg-white flex flex-col gap-6 rounded-md text-lg animate-[fadeIn_0.5s] py-6 px-8'>
             <div className='flex justify-between items-center text-black'>
                 <h2>{currentState}</h2>
                 <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt='cross button' className='w-4 cursor-pointer'/>
@@ -33,7 +66,7 @@ const LoginPopup = ({setShowLogin}) => {
                 <input type='email' name='email' onChange={onChangeHandler} value={data.email} placeholder='Your Email id' required className='outline-none border-2 border-solid border-purple-600 rounded-md'/>
                 <input type='password' name='password' onChange={onChangeHandler} value={data.password} placeholder='Password' required className='outline-none border-2 border-solid border-purple-600 rounded-md'/>
             </div>
-            <button className='border-none p-2.5 rounded-md text-white bg-red-700 text-sm'>
+            <button type='submit' className='border-none p-2.5 rounded-md text-white bg-red-700 text-sm'>
                 {
                     currentState==="Sign Up" ? "Create account" : "Login"
                 }
