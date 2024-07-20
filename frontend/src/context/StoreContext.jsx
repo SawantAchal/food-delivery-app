38cdd6fbaa0f62917ae771255a30196bfbea3ba0
SawantAchal <achals1718@gmail.com>
2024-07-21 02:52:11 +0530

    created a function to load foodlist from database

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState ,useEffect } from "react";
-import { food_list } from "../assets/assets";
+// import { food_list } from "../assets/assets";
+import axios from 'axios'
 
 export const StoreContext = createContext(null)
 
@@ -9,6 +10,7 @@ const StoreContextProvider = (props) => {
     const url ="http://localhost:4000"
     //crested state to store token in loginform 
     const [token , setToken] = useState("")
+    const [food_list ,setFoodList] = useState([])
 
     // to add cart item functionality
     const addToCart = (itemId) => {
@@ -41,18 +43,31 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
-    //logic for - after a refresh page data should be remain in local storage .
-    useEffect(() => {
-        if(localStorage.getItem("token")){
-            setToken(localStorage.getItem("token"))
-        }
-    },[])
+
 
     //to check product is add in cart or not 
     // useEffect(() => {
     //   console.log(cartItems);
     // }, [cartItems])
     
+    // function to fetch food item from data base in the state 
+    const fetchFoodList = async () => {
+        //call api
+        const response = await axios.get(url+"/api/food/list")
+        setFoodList(response.data.data)
+        // console.log(response)
+    }
+
+    useEffect(() => {
+        async function loadData(){
+            await fetchFoodList();
+        }
+        //logic for - after a refresh page data should be remain in local storage .
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"))
+        }
+        loadData()
+    },[])
 
     const contextValue = {
         //mount food list from assets to use in any component
