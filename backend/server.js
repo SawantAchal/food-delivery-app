054456add7602a44a7e6d942d73f2e9b23d2a3c6
SawantAchal <achals1718@gmail.com>
2024-07-22 22:00:55 +0530

    added cart api

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js';
+
+//app config
+const app = express();
+const port = 4000;
+
+//middleware 
+app.use(express.json());
+app.use(cors());
+
+//db connect
+connectDB();
+
+//api endpoint
+app.use('/api/food',foodRouter);
+//mount upload folder to the 'images' endpoint.
+app.use('/images',express.static('uploads'))
+app.use('/api/user' , userRouter)
+app.use('/api/cart',cartRouter)
+
+// request the data for server
+app.get('/' , (req , res) => {
+    res.send("API Working")
+});
+
+// to run express server
+app.listen(port , () => {
+    console.log(`server started on http://localhost:${port}`)
+});
+
