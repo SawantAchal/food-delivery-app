74dd10aa1f1e58c88bd7c5fe3494f592115457d7
SawantAchal <achals1718@gmail.com>
2024-07-22 21:50:20 +0530

    created routes for the cart of individual user

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {addToUserCart ,removeItemFromUserCart ,getCartdetailFromUserCart} from '../controllers/cartController.js'
+import authMiddleware from '../middleware/auth.js';
+
+const cartRouter =  express.Router();
+
+//api endpoint
+cartRouter.post('/addtousercart' ,authMiddleware,addToUserCart)
+cartRouter.post('/removefromcart' ,authMiddleware,removeItemFromUserCart)
+cartRouter.get('/getcartdetails' ,authMiddleware,getCartdetailFromUserCart)
+
+export default cartRouter;
