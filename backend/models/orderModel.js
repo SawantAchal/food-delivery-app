0cc9e49bb1665560bb3cf65380518328a46a32f5
SawantAchal <achals1718@gmail.com>
2024-07-23 16:32:03 +0530

    created the schema and order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+//schema for order
+const orderSchema = new mongoose.Schema({
+    userId:{type:String,required:true},
+    items:{type:Array,required:true},
+    amount:{type:Number,required:true},
+    address:{type:Object,required:true},
+    status:{type:String,default:"Food Processing"},
+    date:{type:Date,default:Date.now()},
+    payment:{type:Boolean,default:false},
+})
+
+//we create the model to create the database in the mongodb
+//order Model
+const orderModel = mongoose.models.order || mongoose.model("order" ,orderSchema);
+
+export default orderModel;
