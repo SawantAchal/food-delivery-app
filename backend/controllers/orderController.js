0f04d7d52373e1ddc2a14cded36a0e20f0d49273
SawantAchal <achals1718@gmail.com>
2024-07-26 20:46:05 +0530

    created af function for verify order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -53,8 +53,26 @@ const placeOrder = async(req, res) => {
 
         res.json({success:true,session_url:session.url})
     } catch (error) {
-        
+        console.log(error)
+        res.json({success:false,message:error})
     }
 }
 
-export {placeOrder};
+const verifyOrder = async (req,res) => {
+    const {orderId,success} = req.body;
+    try {
+        if (success=='true') {
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"paid"})
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false,message:"Not paid"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder,verifyOrder};
