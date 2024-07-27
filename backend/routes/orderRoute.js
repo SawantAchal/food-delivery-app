ba8580ff3bd3813e3b488ab74feec52a6856afea
SawantAchal <achals1718@gmail.com>
2024-07-27 16:52:16 +0530

    created route for userorder

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import authMiddleware from '../middleware/auth.js'
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrder, verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 //api endpoint
 orderRouter.post("/placeorder" , authMiddleware ,placeOrder);
-orderRouter.post("/verifyorder" , authMiddleware ,verifyOrder);
+orderRouter.post("/verifyorder" ,verifyOrder);
+orderRouter.post("/userorders" , authMiddleware ,userOrder);
 
 export default orderRouter;
