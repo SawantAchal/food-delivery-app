ebc3130cc12859e427b03d17cbe1ae1ac8de41b2
SawantAchal <achals1718@gmail.com>
2024-08-22 01:06:58 +0530

    add expired time for token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,13 +13,13 @@ const loginUser = async (req, res) => {
         //to check we get user or not 
         if (!user) {
             //if we not get any user
-            return res.json({success:false, messsage:"user Doesnot exist"})
+            return res.json({success:false, messsage:"user Does not exist"})
         }
         //if getting user then we match password the store password in the db
         const isMatch = await bcrypt.compare(password , user.password)
         //id password is not match 
         if (!isMatch) {
-            return res.json({success:false,messsage:"Invalid credentials"})
+            return res.json({success:false, message:"Invalid credentials"})
         }
 
         //if password is match we gemerated  one token 
@@ -27,13 +27,13 @@ const loginUser = async (req, res) => {
         res.json({success:true,token})
     } catch (error) {
         console.log(error)
-        res.json({success:false,messsage:"Error"})
+        res.json({success:false,message:"Error"})
     }
 }
 
 const createToken = (id) => {
     //generated token
-    return jwt.sign({id} ,process.env.JWT_SECRET)
+    return jwt.sign({id} ,process.env.JWT_SECRET , {expiresIn:'1hr'})
 }
 
 //register user 
@@ -44,15 +44,15 @@ const registerUser = async (req, res) => {
         //check email is already register or not
         const exists = await userModel.findOne({email});
         if (exists) {
-            return res.json({success:false,messsage:"User already exixts"})
+            return res.json({success:false,message:"User already exixts"})
         }
         //validating email format and strong password
         if (!validator.isEmail(email)) {
-            return res.json({success:false,messsage:"Please enter valid email"})
+            return res.json({success:false,message:"Please enter valid email"})
         }
 
         if (password.length<8) {
-            return res.json({success:false,messsage:"Please enter strong password"})
+            return res.json({success:false,message:"Please enter strong password"})
         }
 
         //to encrypt the passowrd
@@ -72,7 +72,7 @@ const registerUser = async (req, res) => {
         res.json({success:true,token})
     } catch (error) {
         console.log(error)
-        res.json({success:false,messsage:"Error"})
+        res.json({success:false,message:"Error"})
     }
 }
 
